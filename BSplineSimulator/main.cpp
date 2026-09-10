76472e469fe26dfec0d92d20cc2a5b4d5c8eae40
Minso <minso9187@gmail.com>
2026-09-10 23:35:05 +0200

    Added imgui files

--- BSplineSimulator/main.cpp
+++ BSplineSimulator/main.cpp
@@ -4,6 +4,10 @@
 #include <vector>
 #include "Camera.h"
 #include "BSplineCurve.h"
+#include "imgui.h"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl2.h"
 
 int main()
 {
